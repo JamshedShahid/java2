29dcb459a9b22b2318f1519fb8ccd303022c77b7
Muhaamd Jamshed <eyes0033@gmail.com>
2021-11-12 02:42:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,304 @@
+// alert("Jamshed");
+
+// Q===========
+
+// var firstName= prompt("Enter First Name")
+
+// var lastName= prompt("Enter Last Name")
+// var fullName = (firstName + " "+lastName)
+// document.write(fullName + "<br>" + "Assalamualikum");
+
+
+// Q=======
+// var phone= prompt("Enter Your Phone Name")
+
+// console.log(phone.length)
+
+// Q================
+// var pk = "Pakistan"
+// console.log(pk.indexOf("n"))
+
+
+// Q===========
+// var pk = "Hello World"
+// console.log(pk.lastIndexOf("l"))
+
+
+// Q=========
+// var pk = "Pakistani"
+// console.log(pk.charAt(3))
+
+
+// Q============
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Q==================
+
+// var hydre= "hyder"
+// console.log(hydre)
+
+// var hydre= "hyder"
+// console.log(hydre.replace("hyder","islam"))
+
+
+// var hydre= "hyderabad"
+// console.log(hydre.replace("hyderabad","islamabad"))
+
+
+// Q======
+// var message = "“Ali and Sami are best friends. They play cricket and football together.";
+// for(var i=0; i< message.length;  i++){
+//     if(message.slice(i , i +3)==="and"){
+//         message = message.slice(0,i)+"&"+message.slice(i+3)
+//         // console.log(i)
+//         console.log(message)
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+// var para = "The New Yorker magazine doesn't allow the phrase World War II.  They say it should be the Second World War."
+
+// console.log(para);
+
+
+// for(var i = 0 ; i<para.length ; i++){
+
+//       if( para.slice(i  ,  i + 12)   === "World War II"  ){
+                
+//             para = para.slice(0 , i) + "&"  +  para.slice(i +12) 
+//             console.log( "para==>" , para);
+      
+//         }  
+
+// }
+
+
+
+// Q=========
+// var num1 = "476"
+// Number(num1)
+// document.write(num1)
+
+
+// Q==========
+// var pri = prompt("Enter Name").toLocaleUpperCase()
+// document.write(pri)
+// pri.toUpperCase(pri)
+
+
+// Q==
+
+// var pri = prompt("Enter Name")
+// pri =pri.slice(0,1).toUpperCase()+pri.slice(1)
+// document.write(pri)
+
+
+// Q=========
+
+// var wa = 35.36
+//  var rep =
+// document.write(wa)
+
+// Q=======
+
+
+
+
+// Q===
+// var pr =prompt("Enter Fruits Name").toUpperCase()
+// var arr =["Apple", "Banana", "Mango" ,"Ananas" ,"iceCream"]
+// for(i=0; i<arr.length; i++){
+//  if()){
+//         alert("Cookies is available at index")
+//     }
+
+
+
+// Q=====
+
+
+// var pas = prompt("Enter Passward")
+// if(pas==)
+
+
+
+
+
+
+
+
+// Q========
+
+//  var text = "University of Karachi" ;
+//  var words = text.split('');
+
+// document.write(words)
+
+
+// Q======
+// var para = "The quick brown fox jumps over the lazy dog."
+// count = 2;
+// for (i=0;i<para.length;i++){
+//     if(para.charAt(i)==="the"){
+//         // count++;
+//     }
+//     console.log(count)
+// }
+
+
+
+
+
+// Q====
+
+// var last =prompt("Name")
+// console.log(last.charAt(last.length -1))
+
+// Q==========
+
+// var num=-5
+// var ab= Math.abs(num)
+// document.write(ab)
+
+
+// Q============
+
+
+// var num1 = 532.433
+// var round=Math.round(num1)
+// document.write(round)
+
+
+// var num = 532.433
+// var round=Math.floor(num)
+// console.log(round)
+
+
+// var num1 = 532.433
+// var round=Math.ceil(num1)
+// document.write(round)
+
+
+// Q==========
+
+// var num1 = -2.673
+// var round=Math.round(num1)
+// document.write(round)
+
+
+// var num = -2.673
+// var round=Math.floor(num)
+// console.log(round)
+
+
+// var num1 = -2.673
+// var round=Math.ceil(num1)
+// document.write(round)
+
+
+
+
+
+// Q========
+
+// var random =  Math.random()
+// console.log(random);
+
+
+// Q=====
+// var random = Math.floor(Math.random()* 4 +6)
+// document.write(random)
+
+// var random = Math.floor( Math.random() * 10 + 1)
+// console.log(random);
+
+
+// Q=====
+
+
+// var headUser = prompt("Enter Your name" , "HEAD USER")
+// var tailUser = prompt("Enter Your name" , "TAIL USER")
+
+// var headScore = 0
+// var tailScore = 0
+
+// var toss = Math.floor( Math.random()  * 1 + 2  )
+// console.log(toss);
+
+
+
+
+
+// Q=========
+
+
+// var ran =Math.floor( Math.random()* 100+ 1)
+// document.write( "Random Number is"+ran)
+
+
+// Q======
+
+
+// var num = +prompt("Enter number")
+// if(num==1 || num==10){
+//     alert("WellDone")
+// }
+// else{
+//     alert("enter correct Number")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
